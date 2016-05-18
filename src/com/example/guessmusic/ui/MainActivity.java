b5948e085ae358cfd7f6c049f7273ef66898bb9d
LiuchangDuan <870121425@qq.com>
2016-05-18 11:31:09 +0800

    First commit

---
+++ src/com/example/guessmusic/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.guessmusic.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.guessmusic.R;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
